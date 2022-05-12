120ed0f543d8a85994e3e167159ebde15b76d008
Abolfazl Panahiazar <abolfazl.panahiazar@rambody.me>
2022-05-12 21:31:42 +0430

    Add react-query

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -1,12 +1,19 @@
 import { FC, ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 interface IProviderProps {
   children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const Provider: FC<IProviderProps> = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
 export default Provider;
