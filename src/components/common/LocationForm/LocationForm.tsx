6863b5f6f0e39f5538635ae12b5edeccddd3d372
Abolfazl Panahiazar <abolfazl.panahiazar@rambody.me>
2022-05-16 16:46:23 +0430

    Add react hook form and location page logic

--- src/components/common/LocationForm/LocationForm.tsx
+++ src/components/common/LocationForm/LocationForm.tsx
@@ -1,14 +1,40 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useMutation } from "react-query";
 
+import { Create_Location, Update_Location } from "api";
 import { TextInput, SelectInput, MapInput, ImageInput, Button } from "components/common";
 import { Form } from "./locationForm.styles";
-import { LocationType } from "types";
+import { ILocation, LocationType } from "types";
 
-export const LocationForm: FC = () => {
+interface ILocationFormProps {
+  updatingLocation?: ILocation;
+  isUpdating?: boolean;
+}
+
+export const LocationForm: FC<ILocationFormProps> = ({ isUpdating, updatingLocation }) => {
+  const { register, watch, handleSubmit, setValue, reset } = useForm<ILocation>();
+
+  useEffect(() => {
+    if (isUpdating && updatingLocation?.title) {
+      reset({
+        ...updatingLocation
+      });
+    }
+  }, []);
+
+  const onSubmit: SubmitHandler<ILocation> = (data) => {
+    if (isUpdating) {
+      console.log("edit");
+    } else {
+      console.log("create");
+    }
+  };
+
   return (
-    <Form>
-      <TextInput placeholder="Location Title:" />
-      <SelectInput placeholder="Location Type:">
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <TextInput placeholder="Location Title:" {...register("title")} />
+      <SelectInput placeholder="Location Type:" {...register("type")}>
         <option value={LocationType.HOME}>Home</option>
         <option value={LocationType.BUSINESS}>Business</option>
         <option value={LocationType.FAIR}>Fair</option>
--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,5 +1,9 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
 
+import { Get_Location } from "api";
+import { ILocation } from "types";
 import Container from "components/Container/Container";
 import { LocationForm } from "components/common";
 
@@ -8,9 +12,29 @@ interface ILocationPageProps {
 }
 
 const Location: FC<ILocationPageProps> = ({ isUpdating }) => {
+  const { locationId } = useParams();
+  const [updatingLocation, setUpdatingLocation] = useState<ILocation>();
+
+  const { refetch, isLoading } = useQuery(
+    "location",
+    () => Get_Location(locationId ? +locationId : 1),
+    {
+      enabled: false,
+      onSuccess: (res) => {
+        setUpdatingLocation(res.data);
+      }
+    }
+  );
+
+  useEffect(() => {
+    if (isUpdating && locationId) {
+      refetch();
+    }
+  }, []);
+
   return (
     <Container>
-      <LocationForm />
+      <LocationForm isUpdating={isUpdating} updatingLocation={updatingLocation} />
     </Container>
   );
 };
